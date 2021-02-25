d796556a84510a2d1700f09235cb83b9efb654b7
tushartiwari0172@gmail.com <tushartiwari0172@gmail.com>
2021-02-26 02:39:19 +0530

    removed boilerplates

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+  var [username,setUsername] = useState(null);
+  function usernameHandler(e) {
+    setUsername(e.target.value);
+  }
+  
+  function apiHandler(event) {
+      event.preventDefault();
+      console.log("api called");
+
+      var url="https://www.instagram.com/" + username + "/?__a=1";
+
+      fetch(url, {mode: 'no-cors'})
+      .then(response => response.text())
+      .then(result => console.log(JSON.parse(result,null,'\t')))
+      .catch(error => alert("gaand maraa gya bsdk", error))
+  }
+
   return (
     <div className="App">
       <header>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
+
         <nav className="navbar navbar-dark bg-dark justify-content-between">
           <h1 className="navbar-brand display-2 ml-3" >InstaDp</h1>
-          <form className="form-inline ml-3">
-            <input className="form-control mr-5" style={{width: "20em"}} type="search" placeholder="Enter Instagram Id" aria-label="Search" />
-            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <form className="form-inline mr-lg-3" >
+            <input className="form-control mr-sm-3" 
+              style={{width: "20em"}} 
+              type="search" 
+              onChange={usernameHandler}
+              placeholder="Enter Instagram Id" 
+              aria-label="Search" 
+            />
+            <button 
+                className="btn btn-outline-success my-2 my-sm-0" 
+                onClick={apiHandler} 
+                type="submit">
+              Search
+            </button>
           </form>
         </nav>
       </header>
